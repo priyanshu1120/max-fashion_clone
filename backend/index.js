a0307604ccb8d78f509cccb800e7e05dd4f20b88
Rishav Neogi <nrishav31@gmail.com>
2023-02-21 12:17:00 +0530

    removed async in listen

--- backend/index.js
+++ backend/index.js
@@ -3,21 +3,22 @@ const app = express()
 require("dotenv").config()
 const {connect} = require("./Config/db.js")
 const cors = require("cors")
-const product = require("./routes/product.route.js")
+const product = require("./routes/product.route.js");
+const port=process.env.PORT||4000;
 app.use(express.json())
 app.use(cors({
     origin:"*"
-}))
+}));
 
 app.get("/",(req,res)=>{
     res.send("server run successfully")
 })
 app.use("/product",product)
-app.listen(process.env.PORT,async()=>{
+app.listen(port,()=>{
        try{
-          await connect
-          console.log(`server run on http://localhost:${process.env.PORT} `)
+            connect
+            console.log(`server run on http://localhost:${port} `)
        }catch(err){
-           console.log(err)
+            console.log(err)
        }
 })
